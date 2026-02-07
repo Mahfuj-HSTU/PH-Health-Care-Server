8cabcdf8275ff0c3d69102e25e0cdc9a98d5e472
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-02-08 00:00:01 +0600

    add better auth

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import { routes } from './app/routes'
+import { toNodeHandler } from 'better-auth/node'
+import { auth } from './app/lib/auth'
 
 const app: Application = express()
 // Enable URL-encoded form data parsing
@@ -8,6 +10,7 @@ app.use(express.urlencoded({ extended: true }))
 // Middleware to parse JSON bodies
 app.use(express.json())
 
+app.all('/api/auth/{*any}', toNodeHandler(auth))
 app.use('/api/v1', routes)
 
 // Basic route
