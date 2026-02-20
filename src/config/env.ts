2397b95e3b6b92cf2f427759065d52b688a2b419
Mahfuj-HSTU <rmahfujur16@gmail.com>
2026-02-20 10:17:36 +0600

    update env with more env variables

--- src/config/env.ts
+++ src/config/env.ts
@@ -8,6 +8,12 @@ interface EnvConfig {
 	DATABASE_URL: string
 	BETTER_AUTH_SECRET: string
 	BETTER_AUTH_URL: string
+	ACCESS_TOKEN_SECRET: string
+	REFRESH_TOKEN_SECRET: string
+	ACCESS_TOKEN_EXPIRES_IN: string
+	REFRESH_TOKEN_EXPIRES_IN: string
+	BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: string
+	BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: string
 }
 
 const envConfig = (): EnvConfig => {
@@ -16,7 +22,13 @@ const envConfig = (): EnvConfig => {
 		'NODE_ENV',
 		'DATABASE_URL',
 		'BETTER_AUTH_SECRET',
-		'BETTER_AUTH_URL'
+		'BETTER_AUTH_URL',
+		'ACCESS_TOKEN_SECRET',
+		'REFRESH_TOKEN_SECRET',
+		'ACCESS_TOKEN_EXPIRES_IN',
+		'REFRESH_TOKEN_EXPIRES_IN',
+		'BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN',
+		'BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE'
 	]
 	requiredEnvVariables.forEach((envVar) => {
 		if (!process.env[envVar]) {
@@ -28,7 +40,15 @@ const envConfig = (): EnvConfig => {
 		NODE_ENV: process.env.NODE_ENV || 'development',
 		DATABASE_URL: process.env.DATABASE_URL || '',
 		BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET || '',
-		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL || ''
+		BETTER_AUTH_URL: process.env.BETTER_AUTH_URL || '',
+		ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+		REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
+		ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+		REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
+		BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN: process.env
+			.BETTER_AUTH_SESSION_TOKEN_EXPIRES_IN as string,
+		BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE: process.env
+			.BETTER_AUTH_SESSION_TOKEN_UPDATE_AGE as string
 	}
 }
 
